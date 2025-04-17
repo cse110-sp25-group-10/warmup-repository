1ae3f0e86a8d2a97b48e737825cf8df230929db6
ermge <97652623+kungryan@users.noreply.github.com>
2025-04-17 00:01:56 -0700

    Modify deck-demo for Black Jack
    Add documentations for card component usage
    Add additional containers in html
    Change buttons in html to reflect Black Jack
    Add CSS for deck
    Remove unneeded functions
    Add a function to hit (draw cards)

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-Basic starting point of HTML, CSS, and JS
-Mostly for demonstrative/learning purposes.
+# Documentation
+## Nav
+[Card Component Usage](#card-component-usage)
+
+## Card Component Usage
+SUIT: The suit of the card ("clubs", "diamonds", "spades", "hearts").
+
+Value: The numeric value of the card (1-13).
+### Creating a Card Component in HTML:
+```
+<card-component data-suit="SUIT" data-value="VALUE"></card-component>
+```
+
+### Creating a Card Component in JavaScript:
+```
+const card = document.createElement("card-component");
+card.setAttribute("data-suit", "SUIT");
+card.setAttribute("data-suit", "VALUE");
+target.appendChild(card);
--- scripts/game.js
+++ scripts/game.js
@@ -5,22 +5,18 @@ window.addEventListener("DOMContentLoaded", () => {
     // Main references
     const cardContainer = document.querySelector("card-container");
     const handContainer = document.querySelector("hand-container");
-    const shuffleBtn = document.querySelector("#shuffleBtn");
-    const drawBtn = document.querySelector("#drawBtn");
-    const resetBtn = document.querySelector("#resetBtn");
-    const drawAmountInput = document.querySelector("game-ui-container > input");
-    const drawXBtn = document.querySelector("#drawSomeBtn")
+    const hitBtn = document.querySelector("#hitBtn");
 
-    // initialize the game
+    // Initialize the game
     initGame();
 
-    // add functionality to buttons
-    shuffleBtn.addEventListener("click", shuffleDeck);
-    drawBtn.addEventListener("click", drawCard);
-    resetBtn.addEventListener("click", initGame);
-    drawXBtn.addEventListener("click", drawCards);
+    // Add functionality to buttons
+    hitBtn.addEventListener("click", hitCard);
 
-    // returns a card component
+
+    /** || Card Functions */
+
+    // Returns a card component
     function createCard(suit, value) {
         const card = document.createElement("card-component");
         card.setAttribute("data-suit", suit);
@@ -29,8 +25,18 @@ window.addEventListener("DOMContentLoaded", () => {
         return card;
     }
 
+    // Hit a card
+    function hitCard() {
+        let cardJS = deck.pop();
+        const card = createCard(cardJS['suit'], cardJS['value']);
+        handContainer.appendChild(card);
+        card.setAttribute("class", "flipped");
+    }
 
-    /* Randomize array in-place using Durstenfeld shuffle algorithm */
+
+    /** || Deck Functions */
+
+    // Randomize array in-place using Durstenfeld shuffle algorithm
     function shuffleArray(array) {
         for (let i = array.length - 1; i > 0; i--) {
             let j = Math.floor(Math.random() * (i + 1));
@@ -40,43 +46,9 @@ window.addEventListener("DOMContentLoaded", () => {
         }
     }
 
-    // Displays all the cards in the deck
-    function displayDeck() {
-        cardContainer.replaceChildren();
-        for (let card of deck) {
-            cardContainer.append(createCard(card["suit"], card["value"]));
-        }
-    }
+    /** || Game Functions */
 
-
-    // Shuffles the deck
-    function shuffleDeck() {
-        shuffleArray(deck);
-        displayDeck();
-    }
-
-    // draw a card
-    function drawCard() {
-        deck.pop();
-        const card = cardContainer.lastChild;
-        handContainer.appendChild(card);
-        card.setAttribute("class", "flipped");
-    }
-
-    // draw some amount of cards
-    function drawCards() {
-        const inputAmount = drawAmountInput.value*1;
-        console.log(inputAmount);
-        if (!isNaN(inputAmount)) {
-            if (inputAmount <= deck.length && inputAmount > 0) {
-                for (let i = 0; i < inputAmount; i++) {
-                    drawCard();
-                }
-            }
-        }
-    }
-
-    // initialize the game with an empty hand and a deck in new deck order.
+    // initialize the game with an empty hand and a shuffled deck.
     function initGame() {
         deck.length = 0;
         // initialize deck
@@ -89,7 +61,7 @@ window.addEventListener("DOMContentLoaded", () => {
                 deck.push(card);
             }
         }
-        displayDeck();
+        shuffleArray(deck);
         handContainer.replaceChildren();
     }
 
