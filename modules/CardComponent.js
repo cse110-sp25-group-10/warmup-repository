5b86caff6dd2ad5bd4d3ebc4c38a212a3ecf05ea
JohnsonUCSD <joc036@ucsd.edu>
2025-04-18 23:22:30 -0700

    Added Function for suit, value, filename

--- modules/CardComponent.js
+++ modules/CardComponent.js
@@ -1,6 +1,36 @@
 class CardComponent {
   constructor(cardNumber) {
     this.cardNumber = cardNumber;
-    this.value = (this.cardNumber % 13) + 1;
+    this.faceup = false;
   }
+
+  getSuit(cardNumber){
+    if (this.cardNumber <= 13) return 'spades';
+    if (this.cardNumber <= 26) return 'clubs';
+    if (this.cardNumber <= 39) return 'hearts';
+    return 'diamonds';
+  }
+
+  getValue() {
+    return ((this.cardNumber - 1) % 13) + 1;
+  }
+
+  getCardFilename() {
+    const suit = this.getSuit();
+    const value = this.getValue();
+    
+    // Convert value to card name (1 = ace, 11 = jack, etc.)
+    let valueName;
+    switch (value) {
+      case 1: valueName = 'A'; break;
+      case 11: valueName = 'J'; break;
+      case 12: valueName = 'Q'; break;
+      case 13: valueName = 'King'; break;
+      default: valueName = value;
+    }
+    
+    return `${suit}_${valueName}.png`;
+  }
+
+
 }
